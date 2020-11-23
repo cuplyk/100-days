1108d889874e9bfb5480427e75109307e3a6be6a
cuplyk <66882817+cuplyk@users.noreply.github.com>
2020-11-23 22:24:27 +0100

    Variable and Band Name Generator

---
+++ day-1-printing.py
@@ -0,0 +1,22 @@
+# a = input("a: ")
+# b = input("b: "0)
+# print("a", b)
+# print("b", a)
+
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+# print("a:", a)
+# print("b:", b)
+
+
+# BAND NAME GENERATOR 
+print("Welcome to the band name generator.")
+user_name = input("Enter the name of the city where you grew up: \n")
+pet_name = input("Enter the name of your pet: \n")
+
+print("You band name could be " + user_name + " " + pet_name)
+
